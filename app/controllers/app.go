15035aa5fe80314488714393b9bfc55ffd505ce2
kwonhee <s10hotkkh@nate.com>
2015-11-25 21:31:20 +0900

    no message

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -9,3 +9,26 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+func (c App) Me() revel.Result {
+	name := "kkh"
+	return c.Render(name)
+}
+
+func (c App) Menumber(id int) revel.Result {
+	
+	return c.Render(id)
+}
+
+func (c App) Mepost() revel.Result {
+	
+	return c.Render()
+}
+
+func (c App) Mesetpost() revel.Result {
+	password := c.Params.Get("password")
+	if password == "1234" {
+		return c.Redirect(App.Index)
+	}
+	return c.Redirect(App.Mepost)
+}
